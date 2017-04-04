02172f1c6493e70a7a0b8d4b35ddfcdc112d68ff
febrianimanda <febrian.imanda@gmail.com>
2017-04-04 17:50:20 +0700

    adding printout function

--- iofile.py
+++ iofile.py
@@ -5,27 +5,33 @@ import csv, pickle, ast
 datasetDir = 'dataset/'
 exportDir = 'exported/'
 
+def printOut(name, mode='Load'):
+	print mode + "\t| " + name + " successfull"
+
 def saveListToFile(filename, myList):
 	thefile = open(exportDir+filename, 'w')
 	for item in myList:
 		print>>thefile, item
-	print "Saving file successfully"
+	printOut(filename, mode='Save to txt')
 	thefile.close()
 
 def readListFromFile(filename):
 	f = open(exportDir+filename,'r')
 	myList = [ast.literal_eval(line) for line in f]
 	f.close()
+	printOut(filename)
 	return myList
 
 def saveListToPickle(filename, myList):
 	with open(exportDir+filename,'wb') as f:
 		pickle.dump(myList, f)
-	print "Saving with pickle successfully"
+	printOut(filename, mode='Save to pickle')
 
 def readListFromPickle(filename):
 	with open('exported/'+filename,'rb') as f:
-		return pickle.load(f)
+		data = pickle.load(f)
+		printOut(filename)
+		return data
 
 def saveDictToCSV(filename, fieldnames, data):
 	with open(exportDir+filename, 'wb') as csvfile:
@@ -33,10 +39,11 @@ def saveDictToCSV(filename, fieldnames, data):
 		writer.writeheader()
 		for item in data:
 			writer.writerow(item)
-	print "%s.csv | Save successfull" % (filename)
+	printOut(filename, mode='Save to csv')
 
 def readDictFromCSV(filename):
 	f = open(exportDir+filename,'rb')
 	reader = csv.DictReader(csvfile)
 	f.close()
+	printOut(filename)
 	return reader

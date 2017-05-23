c1f266cbcfb46b0692d58c75f1e06d25b3f9ba46
febrianimanda <febrian.imanda@gmail.com>
2017-05-24 00:08:11 +0700

    add cross validation

--- classification.py
+++ classification.py
@@ -1,7 +1,8 @@
-import iofile, numpy as np, pickle
+import iofile, numpy as np, pickle, time
 from sklearn import svm
 # import matplotlib.pyplot as plt
 
+startTime = time.time()
 
 def processingData(rawData):
 	data = []
@@ -15,7 +16,8 @@ def processingData(rawData):
 		data.append(obj)
 	return data, target
 
-def confussionMatrix(model, testData, target):
+def confussionMatrix(model, testData, testTarget):
+	print "== Processing Confussion Matrix =="
 	Z = model.predict(testData)
 	cm = {
 		'tp': 0, 'fp': 0,
@@ -23,10 +25,10 @@ def confussionMatrix(model, testData, target):
 	}
 	for ix in range(len(Z)):
 		if Z[ix] == 1:
-			if target[ix] == Z[ix]: cm['tp'] += 1
+			if testTarget[ix] == Z[ix]: cm['tp'] += 1
 			else: cm['fp'] += 1
 		else:
-			if target[ix] == Z[ix]: cm['tn'] += 1
+			if testTarget[ix] == Z[ix]: cm['tn'] += 1
 			else: cm['fn'] += 1
 	print "Confussion Matrix: ",cm
 	return cm
@@ -40,6 +42,15 @@ def accuracy(confMatrix):
 def sensitivity(confMatrix):
 	return float(confMatrix['tp']) / float(confMatrix['tp'] + confMatrix['fn'])
 
+def precision(confMatrix):
+	return float(confMatrix['tp']) / float(confMatrix['tp'] + confMatrix['fp'])
+
+def recall(confMatrix):
+	return sensitivity(confMatrix)
+
+def f1score(precision, recall):
+	return 2 * (float(precision*recall) / (precision + recall))
+
 def processBuyingSession(rawData, paramData, model):
 	Z = model.predict(paramData)
 	data =  []
@@ -51,18 +62,76 @@ def processBuyingSession(rawData, paramData, model):
 	iofile.savePickle('buys-1.pkl', data)
 	return data
 
+def crossValidation(data, target, kernel='linear'):
+	print "=== Processing 10-fold Cross Validation ==="
+	k = 10
+	foldLength = float(len(data)) / k
+	# a,b adalah batas awal dan akhir untuk testing data
+	for ix in range(k):
+		print "Processed Time:", (time.time() - startTime)
+		print "Fold %d kernel %s" % (ix, kernel)
+		a, b = ix * foldLength, (ix+1) * foldLength
+		testData, testTarget = data[a:b], target[a:b]
+		trainData, traiTarget = [], []
+		for i in range(k):
+			if i != ix:
+				awal, akhir = i * foldLength, (i+1) * foldLength
+				trainData.extend(data[awal:akhir])
+				trainTarget.extend(target[awal:akhir])
+
+		X, y = np.array(trainData), np.array(trainTarget)
+
+		if kernel == 'sigmoid':
+			svc = svm.SVC(kernel=kernel, C=.01, gamma=.5, coef0=0).fit(X,y)
+		elif kernel == 'rbf':
+			svc = svm.SVC(kernel=kernel, C=10, gamma=.5).fit(X,y)
+		elif kernel == 'poly':
+			svc = svm.SVC(kernel=kernel, C=.01, gamma=1, degree=3, coef0=10).fit(X,y)
+		else:
+			kernel = 'linear'
+			svc = svc.SVC(kernel=kernel, C=.1).fit(X,y)
+
+		filename = 'svc-'+kernel
+		iofile.savePickle('pengujian/'+filename+'-fold'+ix+'.pkl')
+
+		savePengujian(ix, svc, )
+
+
+def savePengujian(ix, svc, trData, trTarget, tData, tTarget, filename):
+	confMatrix = confussionMatrix(svc, tData, tTarget)
+	erRate = errorRate(confMatrix)
+	acc = accuracy(confMatrix)
+	sens = sensitivity(confMatrix) #sensitivity atau recall
+	precision = precision(confMatrix)
+	f1score = f1score(precision, sens)
+
+	print "Fold:", ix
+	print "Error Rate: ", erRate
+	print "Accuracy: ", acc
+	print "Sensitivity/Recall: ", sens
+	print "Precision: ", precision
+	print "F1 Score:", f1score
+
+	if iofile.isFileExist('pengujian/'+filename+'.csv'):
+		dataUji = iofile.readDictFromCSV('pengujian/'+filename+'.csv')
+	else:
+		dataUji = []
+
+	pengujian_keys = ['fold', 'err', 'acc', 'sens', 'precision', 'f1']
+	pengujian_values = [ix, erRate, acc, sens, precision, f1score]
+	obj = dict(zip(pengujian_keys, pengujian_values))
+	dataUji.extend(obj)
+
+	iofile.saveDictToCsv('pengujian/'+filename+'.csv')
+	fileheader = ['details', 'duration', 'page_per_time']
+	iofile.saveListToCsv('pengujian/training-'+filename+'-fold'+ix+'.csv', fileheader, trData)
+	iofile.saveListToCsv('pengujian/testing-'+filename+'-fold'+ix+'.csv', fileheader, tTarget)
+
 rawData = iofile.readPickle('clicks-combine-2.pkl')
+print "Time process:", (time.time() - startTime)
 data, target = processingData(rawData)
-X = np.array(data)
-y = np.array(target)
 
-# svc = svm.SVC(kernel='linear', C=0.1, verbose=10).fit(X,y)
-# iofile.savePickle('svc-pickle.pkl')
-svc = iofile.readPickle('svc-pickle-2.pkl')
+print "Time process:", (time.time() - oldTime)
+crossValidation(data, target)
 
-confMatrix = confussionMatrix(svc, data, target)
-print "Error Rate: ",errorRate(confMatrix)
-print "Accuracy: ",accuracy(confMatrix)
-print "Sensitivity: ",sensitivity(confMatrix)
-
-processBuyingSession(rawData, data, svc)
+# processBuyingSession(rawData, data, svc)
